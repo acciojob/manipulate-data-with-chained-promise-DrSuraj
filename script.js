f5f9b384745db05360e90c152b289d603d9015cd
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-02-20 15:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 function manipulateData(inputArray) {
+
   return new Promise((resolve) => {
+
     setTimeout(() => {
+
       resolve(inputArray);
+
     }, 3000);
+
   })
+
   .then((data) => {
+
     return new Promise((resolve) => {
+
       setTimeout(() => {
+
         const filteredArray = data.filter((num) => num % 2 === 0);
+
         document.getElementById('output').innerText = `Filtered Result: [${filteredArray}]`;
-        resolve(filteredArray);
-      }, 1000); // Changed to 1000 milliseconds for the first promise
+
+        resolve(filteredArray);  
+
+      }, 1000);
+
     });
+
   })
+
   .then((filteredArray) => {
+
     const multipliedArray = filteredArray.map((num) => num * 2);
+
     document.getElementById('output').innerText = `Final Result: [${multipliedArray}]`;
+
     return multipliedArray;
+
   });
+
 }
 
 const inputArray = [1, 2, 3, 4];
+
 manipulateData(inputArray);

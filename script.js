5bc41af940ef177351df723022f7fb894d4feeb7
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-02-23 18:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
 function manipulateData(inputArray) {
-
-  return new Promise((resolve) => {
-
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-
       resolve(inputArray);
-
     }, 3000);
-
   })
-
-  .then((data) => {
-
-    return new Promise((resolve) => {
-
+  .then(array => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-
-        const filteredArray = data.filter((num) => num % 2 === 0);
-
-        document.getElementById('output').innerText = `Filtered Result: [${filteredArray}]`;
-
-        resolve(filteredArray);  
-
+        const evenNumbers = array.filter(num => num % 2 === 0);
+        document.getElementById("output").innerText = evenNumbers.join(", ");
+        resolve(evenNumbers);
       }, 1000);
-
     });
-
   })
-
-  .then((filteredArray) => {
-
-    const multipliedArray = filteredArray.map((num) => num * 2);
-
-    document.getElementById('output').innerText = `Final Result: [${multipliedArray}]`;
-
-    return multipliedArray;
-
+  .then(evenNumbers => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const multipliedNumbers = evenNumbers.map(num => num * 2);
+        document.getElementById("output").innerText = multipliedNumbers.join(", ");
+        resolve(multipliedNumbers);
+      }, 2000);
+    });
   });
-
 }
 
+// Usage example
 const inputArray = [1, 2, 3, 4];
-
 manipulateData(inputArray);
